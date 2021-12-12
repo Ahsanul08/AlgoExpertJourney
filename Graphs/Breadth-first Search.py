d3b54023ca1b862cb67d15e4d34099cff3f75d53
S M Ahasanul Haque <yourgitemail@example.com>
2021-12-12 15:07:19 +0100

    day 2, problem 1

---
+++ Graphs/Breadth-first Search.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def breadthFirstSearch(self, array):
+        # Write your code here.
+        queue = [self]
+		
+		while queue:
+			node = queue.pop(0)
+			array.append(node.name)
+			
+			for child in node.children:
+				queue.append(child)
+		return array
+
